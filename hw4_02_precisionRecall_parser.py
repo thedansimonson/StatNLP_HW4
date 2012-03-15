ac7dd6867d6e10bcc81b44aa1e6f7fc483b44470
Dan Simonson <dolus.occuli@gmail.com>
2012-03-14 23:56:42 -0400

    Added start/stop time. Fixed keyboardInterrupts

--- hw4_02_precisionRecall_parser.py
+++ hw4_02_precisionRecall_parser.py
@@ -1,6 +1,7 @@
 from pickle import load, dump
 import sys
 from os import getcwd, walk
+from datetime import datetime
 
 print "Loading parser..."
 parser = load(open("hw4_vitParser.pkl", "r"))
@@ -9,7 +10,7 @@ testSents = map(lambda x: map(lambda y: y[0], x.pos()), testTrees)
 
 #prepare all data for the loop
 testPairs = zip(testSents, testTrees, range(0, len(testTrees)))
-testPairs.sort(key = lambda x: len(x[0]))
+#testPairs.sort(key = lambda x: len(x[0]))
 
 #determine where parsing should continue based on the saved parse files
 parseFolder = getcwd()+"/hw4_vitParses"
@@ -17,13 +18,15 @@ getPaths = lambda x: map(lambda y: x[0]+"/"+y, x[2])
 complexPaths = map(lambda x: x, walk(parseFolder))
 paths = reduce(lambda x,y: x+getPaths(y), [[]]+complexPaths)
 
-for each in paths: print each
-raw_input("ANYKEY BROW")
+resume = len(paths)
+print "Resuming parse at "+str(resume)
 
-for each in testPairs:
+for each in testPairs[resume:]:
 	rawSent, ideal, index = each
 	print "Working on "+str(index)+":\n"+" ".join(rawSent)
-	
+	start = datetime.now()
+	print "Started at "+str(start)
+
 	try:
 		parsed = parser.parse(rawSent)
 		print ideal
@@ -40,9 +43,12 @@ for each in testPairs:
 	
 	#save parse to accumulate for later evaluation
 	#and in case script crashes
-	filename = str(index)+"_"+rawSent[0]+".pkl"
-	dump(parsed, open(parseFolder+"/"+filename, "r"))
-	print "Writing "+filename
 
+	filename = str(index)+"_"+rawSent[0]+".pkl"
+	print "Writing "+filename
+	print "DO NOT EXIT UNTIL NEXT MESSAGE APPEARS"
+	dump(parsed, open(parseFolder+"/"+filename, "w"))
+	print "THIS IS THE NEXT MESSAGE. YOU CAN QUIT IF YOU HAVE TO."
+	print "Time elapsed: "+str(start-datetime.now())
 	print ""
 
